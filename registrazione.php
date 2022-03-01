38f8c870bf7f0a4d3369cc1d1e80dcf37d33d72d
Th3R3alZ4ny <bzaniolo83@gmail.com>
2022-03-01 12:41:13 +0100

    si

--- registrazione.php
+++ registrazione.php
@@ -1,13 +1,15 @@
 <?php
-//recupero dati inseriti nella pagina chiamante
-$usr=$_REQUEST['usr'];
-$pw=$_REQUEST['pw'];
-$cognome=$_REQUEST['cognome'];
-$qualifica=$_REQUEST['qualifica'];
-$nome=$_REQUEST['nome'];
-$mail=$_REQUEST['mail'];
-$telefono=$_REQUEST['telefono'];
-$curriculum=$_FILES['curriculum'];
+$_usr=$_REQUEST['usr'];
+$_pw=$_REQUEST['pw'];
+$_cpw=$_REQUEST['cpw'];
+$_cognome=$_REQUEST['cognome'];
+$_nome=$_REQUEST['nome'];
+$_email=$_REQUEST['email'];
+$_telefono=$_REQUEST['telefono'];
+$_qualifica=$_REQUEST['qualifica'];
+$_area=$_REQUEST['area'];
+
+
 //connessione al database
 $hostname="localhost";
 $username="root";
@@ -17,38 +19,62 @@ $conn=new mysqli($hostname, $username, $password, $dbname);
 //controllo di avvenuta connessione
 if ($conn->connect_error)
 	die("errore di connessione");
+$_query="SELECT * FROM espositori WHERE username='{$_usr}'";
 
-$query="INSERT INTO espositori(username,password,cognome,nome,mail,telefono,curriculum,qualifica) 
-VALUES('($usr)','($pw)','($cognome)','($nome)','($mail)','($telefono)','','($qualifica)'";
-//esecuzione della query tramite il metodo query.La query restituisce una tabella temporanea
-$tabella=$conn->query($query);
-
-print_r($_FILES);
-if(!isset($_FILES['userfile'])||!is_uploaded_file($_FILES['userfile']['lmp_name'])){
-    echo 'Non hai inviato nessun file...';
-    //exit
+$tabella=$conn->query($_query);
+if ($tabella-> num_rows == 1)
+  echo("Utente già esistente");
+else {
+  $_query1="INSERT INTO espositori(username,password,cognome,nome,email,qualifica,area,telefono)
+          VALUES ('{$_usr}','{$_pw}','{$_cognome}','{$_nome}','{$_email}',{$_qualifica},{$_area},'{$_telefono}')";
+echo $_query1;
+$ris=$conn->query($_query1);
+if ($ris)
+  echo"Insert ok";
+else {
+  echo "Errore insert";
+  echo $_query1;
 }
-//percorso della cartella dove mettere i file caricati dagli utenti
-$uploaddir='uploads/';
-//recupero il percorso temporaneo del file
-$userfile_tmp=$_FILES['userfile']['tmp_name'];
-//recupero il nome originale del file caricato
-$userfile_name=$_FILES['userfile']['name'];
 
-$ext_ok=array('html','odt','pdf');
-$temp=explode('.',$_FILES['userfile']['name']);
-$ext=end($temp);
-if(!in_array($ext,$ext_ok)){
-    echo 'Il file ha un estensione non ammessa!';
-    exit;
+
+
+
+#echo $_query;
+
+// per prima cosa verifico che il file sia stato effettivamente caricato
+print_r($_FILES);
+if (!isset($_FILES['userfile']) || !is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+  
+  
+    echo 'Non hai inviato nessun file...';
+  //exit;    
+}
+
+//percorso della cartella dove mettere i file caricati dagli utenti
+
+$uploaddir = 'uploads/';
+
+//Recupero il percorso temporaneo del file
+$userfile_tmp = $_FILES['userfile']['tmp_name'];
+
+//recupero il nome originale del file caricato
+$userfile_name = $_FILES['userfile']['name'];
+
+$ext_ok = array('html', 'odt', 'pdf');
+$temp = explode('.', $_FILES['userfile']['name']);
+$ext = end($temp);
+if (!in_array($ext, $ext_ok)) {
+  echo 'Il file ha un estensione non ammessa!';
+  exit;
 }
 
 //copio il file dalla sua posizione temporanea alla mia cartella upload
-if(move_uploaded_file($userfile_tmp,$uploaddir,$userfile_name)){
-    //se l'operazione è andata a buon fine...
-    echo 'file inviato con successo.';
+if (move_uploaded_file($userfile_tmp, $uploaddir . $userfile_name)) {
+  //Se l'operazione è andata a buon fine...
+  echo 'File inviato con successo.';
 }else{
-    //se l'operazione è fallita
-    echo 'Upload NON valido!'
-}		
+  //Se l'operazione è fallita...
+  echo 'Upload NON valido!'; 
+}
+}
 ?>
