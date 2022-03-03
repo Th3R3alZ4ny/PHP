bb06a09b60a915dacbee0d675727048c6297f4dc
Th3R3alZ4ny <bzaniolo83@gmail.com>
2022-03-03 10:27:45 +0100

    si

--- registrazione.php
+++ registrazione.php
@@ -3,7 +3,11 @@
 $usr=$_REQUEST['usr'];
 $pw=$_REQUEST['pw'];
 $cognome=$_REQUEST['cognome'];
+$nome=$_REQUEST['nome'];
 $qualifica=$_REQUEST['qualifica'];
+$mail=$_REQUEST['mail'];
+$telefono=$_REQUEST['telefono'];
+$curriculim=$_REQUEST['curriculum'];
 //connessione al database
 $hostname="localhost";
 $username="root";
@@ -11,9 +15,38 @@ $password="";
 $dbname="fiera";
 $conn=new mysqli($hostname, $username, $password, $dbname);
 //controllo di avvenuta connessione
-if ($conn->connect_error)
+if ($conn->connect_error){
 	die("errore di connessione");
+}
 
+print_r($_FILES);
+if(!isset($_FILES['userfile'])||!is_uploaded_file($_FILES['userfile']['tmp_name'])){
+    echo 'Non hai inviato nessun file...';
+    //exit
+}
+//percorso della cartella dove mettere i file caricati dagli utenti
+$uploaddir='uploads/';
+//recupero il percorso temporaneo del file
+$userfile_tmp=$_FILES['userfile']['tmp_name'];
+//recupero il nome originale del file caricato
+$userfile_name=$_FILES['userfile']['name'];
+
+$ext_ok=array('html','odt','pdf');
+$temp=explode('.',$_FILES['userfile']['name']);
+$ext=end($temp);
+if(!in_array($ext,$ext_ok)){
+    echo 'Il file ha un estensione non ammessa!';
+    exit;
+}
+
+//copio il file dalla sua posizione temporanea alla mia cartella upload
+if(move_uploaded_file($userfile_tmp,$uploaddir,$userfile_name)){
+    //se l'operazione è andata a buon fine...
+    echo 'file inviato con successo.';
+}else{
+    //se l'operazione è fallita
+    echo 'Upload NON valido!'
+}
 $query="insert ......";
 //esecuzione della query tramite il metodo query.La query restituisce una tabella temporanea
 $tabella=$conn->query($query);
